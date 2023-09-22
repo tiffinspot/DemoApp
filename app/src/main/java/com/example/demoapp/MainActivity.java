422e4c2fa5046fdcc5e6be3c59eb9ee096cfc7c7
Alpesh Desai <ranjyaalpesh@gmail.com>
2023-09-22 18:38:33 +0530

    app-center intigration

---
+++ app/src/main/java/com/example/demoapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.demoapp;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "86668039-72a0-41d3-8b9c-7ed2ff4bc669",
+                Analytics.class, Crashes.class);
+    }
+}
